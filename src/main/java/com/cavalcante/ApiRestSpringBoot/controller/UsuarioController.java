7c77ba3b91089eb58c1e71e3ba52f8148d4b32ad
Bruno1083 <cavalcante1083@gmail.com>
2021-06-13 17:55:39 -0300

    crud

--- src/main/java/com/cavalcante/ApiRestSpringBoot/controller/UsuarioController.java
+++ src/main/java/com/cavalcante/ApiRestSpringBoot/controller/UsuarioController.java
@@ -18,7 +18,7 @@ public class UsuarioController {
     public List findAll(){
         return (List) repository.findAll();
     }
-    @GetMapping(path = "/usuario/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity findById(@PathVariable("id") Integer id){
         return repository.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
@@ -27,5 +27,26 @@ public class UsuarioController {
     @PostMapping
     public UsuarioModel create(@RequestBody UsuarioModel usuarioModel){
         return repository.save(usuarioModel);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity update(@PathVariable("id")Integer id, @RequestBody UsuarioModel usuarioModel){
+        return repository.findById(id)
+                .map(record-> {
+                    record.setNome(usuarioModel.getNome());
+                    record.setLogin(usuarioModel.getLogin());
+                    record.setSenha(usuarioModel.getSenha());
+                    UsuarioModel update = repository.save(record);
+                    return ResponseEntity.ok().body(update);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity <?> delete(@PathVariable Integer id){
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
